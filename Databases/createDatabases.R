1960a5f8b641cfa3d0415aae8baff6ae6a735d2b
catalamarti <marticatalasabate@gmail.com>
2026-05-01 13:51:26 +0100

    create table

--- Databases/createDatabases.R
+++ Databases/createDatabases.R
@@ -1,16 +1,43 @@
 
-cdm <- omock::mockCdmFromDataset(datasetName = "delphi-100k_5.4")
-con <- duckdb::dbConnect(drv = duckdb::duckdb(dbdir = here::here("Databases", "delphi.duckdb")))
-DBI::dbExecute(con, "CREATE SCHEMA results")
-CDMConnector::insertCdmTo(cdm = cdm, to = CDMConnector::dbSource(con, "main"))
-DBI::dbRemoveTable(conn = con, name = "genomic_test")
-DBI::dbRemoveTable(conn = con, name = "target_gene")
-DBI::dbRemoveTable(conn = con, name = "variant_annotation")
-DBI::dbRemoveTable(conn = con, name = "variant_occurrence")
-DBI::dbDisconnect(con = con)
+path <- file.path(tempdir(), "OMOP")
+dir.create(path = path)
 
-cdm <- omock::mockCdmFromDataset(datasetName = "GiBleed")
-con <- duckdb::dbConnect(drv = duckdb::duckdb(dbdir = here::here("Databases", "GiBleed.duckdb")))
-DBI::dbExecute(con, "CREATE SCHEMA results")
-CDMConnector::insertCdmTo(cdm = cdm, to = CDMConnector::dbSource(con, "main"))
-DBI::dbDisconnect(con = con)
+c("delphi" = "delphi-100k_5.4", "GiBleed" = "GiBleed") |>
+  purrr::imap(\(x, nm) {
+    options(timeout = 600)
+    omock::downloadMockDataset(datasetName = x, path = path, overwrite = TRUE)
+
+    datasetPath <- file.path(path, paste0(x, ".zip"))
+    tmpFolder <- file.path(path, "source")
+    dir.create(tmpFolder)
+
+    cli::cli_inform(c(i = "Unziping DB."))
+    utils::unzip(zipfile = datasetPath, exdir = tmpFolder)
+
+    unlink(datasetPath, recursive = TRUE)
+
+    cli::cli_inform(c(i = "Reading {.pkg {x}} tables."))
+    con <- duckdb::dbConnect(drv = duckdb::duckdb(
+      dbdir = here::here("Databases", paste0(nm, ".duckdb"))
+    ))
+    DBI::dbExecute(con, "CREATE SCHEMA results")
+    DBI::dbExecute(con, "SET memory_limit = '2GB'")
+    list.files(path = file.path(path, "source", x), full.names = TRUE) |>
+      purrr::map(\(x) {
+        name <- tools::file_path_sans_ext(x = basename(x))
+        if (!name %in% c("genomic_test", "target_gene", "variant_annotation", "variant_occurrence")) {
+          cli::cli_inform(c(i = "Inserting {.pkg {name}}."))
+          sql <- "CREATE TABLE {name} AS SELECT * FROM read_parquet('{x}')" |>
+            glue::glue() |>
+            as.character()
+          DBI::dbExecute(con, sql)
+        }
+        unlink(x)
+      }) |>
+      invisible()
+    DBI::dbDisconnect(con = con)
+
+    unlink(x = tmpFolder, recursive = TRUE)
+  })
+
+unlink(path, recursive = TRUE)
