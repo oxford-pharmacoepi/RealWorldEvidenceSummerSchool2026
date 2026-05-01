c69ecb73b7a8628288a960985e8b10a328604a7d
catalamarti <marticatalasabate@gmail.com>
2026-05-01 13:12:38 +0100

    create databases

---
+++ Databases/README.md
@@ -0,0 +1,4 @@
+# Delphi database
+
+Placeholder for the **delphi** database. If you need to create (or recreate) the
+database please run the `createDatabases.R` code.
---
+++ Databases/createDatabases.R
@@ -0,0 +1,16 @@
+
+cdm <- omock::mockCdmFromDataset(datasetName = "delphi-100k_5.4")
+con <- duckdb::dbConnect(drv = duckdb::duckdb(dbdir = here::here("Databases", "delphi.duckdb")))
+DBI::dbExecute(con, "CREATE SCHEMA results")
+CDMConnector::insertCdmTo(cdm = cdm, to = CDMConnector::dbSource(con, "main"))
+DBI::dbRemoveTable(conn = con, name = "genomic_test")
+DBI::dbRemoveTable(conn = con, name = "target_gene")
+DBI::dbRemoveTable(conn = con, name = "variant_annotation")
+DBI::dbRemoveTable(conn = con, name = "variant_occurrence")
+DBI::dbDisconnect(con = con)
+
+cdm <- omock::mockCdmFromDataset(datasetName = "GiBleed")
+con <- duckdb::dbConnect(drv = duckdb::duckdb(dbdir = here::here("Databases", "GiBleed.duckdb")))
+DBI::dbExecute(con, "CREATE SCHEMA results")
+CDMConnector::insertCdmTo(cdm = cdm, to = CDMConnector::dbSource(con, "main"))
+DBI::dbDisconnect(con = con)
