404b9e6781f5fa6fb28407690ecf8623c940016c
catalamarti <marticatalasabate@gmail.com>
2026-05-01 14:14:05 +0100

    Update createDatabases.R

--- Databases/createDatabases.R
+++ Databases/createDatabases.R
@@ -14,6 +14,8 @@ c("delphi" = "delphi-100k_5.4", "GiBleed" = "GiBleed") |>
     cli::cli_inform(c(i = "Unziping DB."))
     utils::unzip(zipfile = datasetPath, exdir = tmpFolder)
 
+    print(list.files(tmpFolder, recursive = TRUE))
+
     unlink(datasetPath, recursive = TRUE)
 
     cli::cli_inform(c(i = "Reading {.pkg {x}} tables."))
@@ -22,7 +24,7 @@ c("delphi" = "delphi-100k_5.4", "GiBleed" = "GiBleed") |>
     ))
     DBI::dbExecute(con, "CREATE SCHEMA results")
     DBI::dbExecute(con, "SET memory_limit = '2GB'")
-    list.files(path = file.path(path, "source", x), full.names = TRUE) |>
+    list.files(path = tmpFolder, full.names = TRUE, recursive = TRUE) |>
       purrr::map(\(x) {
         name <- tools::file_path_sans_ext(x = basename(x))
         if (!name %in% c("genomic_test", "target_gene", "variant_annotation", "variant_occurrence")) {
@@ -41,3 +43,12 @@ c("delphi" = "delphi-100k_5.4", "GiBleed" = "GiBleed") |>
   })
 
 unlink(path, recursive = TRUE)
+
+con <- duckdb::dbConnect(drv = duckdb::duckdb(dbdir = here::here("Databases", "delphi.duckdb")))
+cdm <- CDMConnector::cdmFromCon(
+  con = con,
+  cdmSchema = "main",
+  writeSchema = "results",
+  writePrefix = "os_"
+)
+CDMConnector::cdmDisconnect(cdm = cdm)
